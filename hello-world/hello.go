1f4d732e70f96d4845f8018d3d7fb7a19cd3d95a
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2025-01-22 16:56:23 +0000

    completed hello world tests and refactor

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		return "Hello, World!"
 	}
-	return "Hello, " + name + "!"
+	prefix := englishPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	}
+
+	return prefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("Levi"))
+	fmt.Println(Hello("Levi", ""))
 }
